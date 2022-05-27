2363a359cb9168495285f15a2db67e810eba68d7
BA_CalderonMorales <tbcm640@gmail.com>
2022-05-26 20:52:31 -0500

    Code cleanup - tab spacing.

--- experiments/pages/experiment-one/experiment-one.js
+++ experiments/pages/experiment-one/experiment-one.js
@@ -1,35 +1,35 @@
 import React, { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
 import styles from "../../styles/ExperimentOne.module.css";
-import Box from "../common/components/Box.jsx";
 
 // Common files
+import Box from "../common/components/Box.jsx";
 import Floor from "../common/components/Floor.jsx";
 import LightBulb from "../common/components/LightBulb.jsx";
 import OrbitControls from "../common/components/OrbitControls";
 import Draggable from "../common/components/DragControls";
 
 const ExperimentOne = () => {
-return (
-    <div style={{ width: "100vw", height: "100vh" }}>
-        <Canvas
-        shadows={true}
-        className={styles.canvas}
-        camera={{
-        position: [-6, 7, 7],
-        }}
-        >
-            <ambientLight color={"white"} intensity={0.3} />
-            <LightBulb position={[0, 3, 0]} />
-            <Suspense fallback={null}>
-                <Draggable>
-                    <Box rotateX={3} rotateY={0.2} />
-                </Draggable>
-            </Suspense>
-            <OrbitControls />
-            <Floor position={[0, -3, 0]} />
-        </Canvas>
-    </div>
+    return (
+        <div style={{ width: "100vw", height: "100vh" }}>
+            <Canvas
+            shadows={true}
+            className={styles.canvas}
+            camera={{
+            position: [-6, 7, 7],
+            }}
+            >
+                <ambientLight color={"white"} intensity={0.3} />
+                <LightBulb position={[0, 3, 0]} />
+                <Suspense fallback={null}>
+                    <Draggable>
+                        <Box rotateX={3} rotateY={0.2} />
+                    </Draggable>
+                </Suspense>
+                <OrbitControls />
+                <Floor position={[0, -3, 0]} />
+            </Canvas>
+        </div>
     );
 };
 
